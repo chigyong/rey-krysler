5d406c2d805f9ed7d22b4a4b5d6bc372bc0cca41
Rey  Baterbonia <poochinng@gmail.com>
2023-01-30 23:21:23 +0800

    init

---
+++ script.js
@@ -0,0 +1,15 @@
+const btn = document.getElementsByClassName('menu')[0]
+const profile = document.getElementsByClassName('profile')[0]
+
+btn.addEventListener('click', () => {
+    profile.classList.toggle('active')
+    btn.classList.toggle('active')
+})
+
+//Animate rain 
+const rain = document.getElementsByClassName('tuyu')[0];
+const container = document.getElementsByClassName('container')[0];
+
+rain.addEventListener('click', () => {
+    container.classList.toggle('active')
+})
